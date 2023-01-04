b75c50b9c609fc94f55e91f1f9b9fcb9081144e0
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-01-04 17:43:43 +0200

    Prepare database context
    Create ApplicationDbContext, add service in program and connection string

--- CleanArchitectureTraining/Program.cs
+++ CleanArchitectureTraining/Program.cs
@@ -1,7 +1,18 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+        options.UseSqlServer(
+            connectionString,
+            x => x.MigrationsAssembly("Infrastructure")));
 
 var app = builder.Build();
 
---
+++ Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
