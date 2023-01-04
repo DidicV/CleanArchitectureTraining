36f1d2cda91bd0ec3ff63066a4aaa40c975381a1
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-01-04 22:53:29 +0200

    Implement Application layer

---
+++ Application/Implementations/BookService.cs
@@ -0,0 +1,41 @@
+﻿using Application.Interfaces;
+using Domain;
+using Infrastructure.Interfaces;
+
+namespace Application.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public void CreateBook(Book book)
+        {
+            _bookRepository.CreateBook(book);
+        }
+
+        public List<Book> GetBooks()
+        {
+            return _bookRepository.GetBooks();
+        }
+
+        public Book? GetBookById(Guid id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _bookRepository.UpdateBook(book);
+        }
+
+        public void DeleteBook(Book book)
+        {
+            _bookRepository.DeleteBook(book);
+        }
+    }
+}
---
+++ Application/Interfaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+
+namespace Application.Interfaces
+{
+    public interface IBookService
+    {
+        void CreateBook(Book book);
+
+        List<Book> GetBooks();
+
+        Book? GetBookById(Guid id);
+
+        void UpdateBook(Book book);
+
+        void DeleteBook(Book book);
+    }
+}
