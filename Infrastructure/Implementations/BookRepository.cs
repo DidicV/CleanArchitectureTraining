71276fe65163342acdc88b946fadb9a9a2e1cf4a
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-01-04 22:51:46 +0200

    Implement Infrastructure layer

---
+++ Infrastructure/Implementations/BookRepository.cs
@@ -0,0 +1,43 @@
+﻿using Domain;
+using Infrastructure.Interfaces;
+
+namespace Infrastructure.Implementations
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateBook(Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        public List<Book> GetBooks()
+        {
+            return _context.Books.ToList();
+        }
+
+        public Book? GetBookById(Guid id)
+        {
+            return _context.Books.Find(id);
+        }
+
+        public void UpdateBook(Book book)
+        {
+            _context.Books.Update(book);
+            _context.SaveChanges();
+        }
+
+        public void DeleteBook(Book book)
+        {
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Infrastructure/Interfaces/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+
+namespace Infrastructure.Interfaces
+{
+    public interface IBookRepository
+    {
+        void CreateBook(Book book);
+
+        List<Book> GetBooks();
+
+        Book? GetBookById(Guid id);
+
+        void UpdateBook(Book book);
+
+        void DeleteBook(Book book);
+    }
+}
