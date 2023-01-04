28c5ba322a43c321f6cfade1a230e483f8449523
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-01-04 17:22:49 +0200

    Add Book entity

---
+++ Domain/Book.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Author { get; set; } = string.Empty;
+    }
+}
